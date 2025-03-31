0388307565fd6f13d12cd82fd6ce9ed683c22268
TatyanaGTV <tatyana_golikova@mail.ru>
2025-03-31 22:50:17 +0300

    changes

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+
+module.exports = (env) => {
+    return {
+        mode: env.mode ?? 'development',
+        entry: './src/gameprocess.js',
+        output: {
+            filename: 'main.js',
+            path: path.resolve(__dirname, 'build'),
+            clean: true,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: ["style-loader", "css-loader"],
+                },
+                {
+                    test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                    type: 'asset/resource',
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: 'assets/images/[name].[ext]', // Измените путь и формат по необходимости
+                                outputPath: 'assets/images', // Папка, в которую будут выводиться изображения
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+        plugins: [new HtmlWebpackPlugin({
+            template: './index.html',
+
+        }),
+            new CopyPlugin({
+                patterns: [
+                    { from: "src", to: "src" },
+                    { from: "assets", to: "assets" },
+                ],
+            }),
+        ],
+
+    };
+}
